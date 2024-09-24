180e9cfd85c13dae12c87b6a4c80e057b7e10580
zawisha <ogniemimechem@gmail.com>
2024-09-24 06:17:33 +0300

    api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -46,7 +46,7 @@ class ApiController extends Controller
     {
         $data = $request->all();
         //Storage::put('messages1.txt', json_encode($data, JSON_PRETTY_PRINT));
-       // $data=json_encode($data, JSON_PRETTY_PRINT);
+        $data=json_encode($data, JSON_PRETTY_PRINT);
         foreach($data[0] as $oneRow)
         {
             $this->readyResults->addResult($oneRow);
