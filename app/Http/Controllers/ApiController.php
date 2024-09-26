018b3153a40f379ecc60d8b2225c05d09547cdfe
zawisha <ogniemimechem@gmail.com>
2024-09-26 13:39:51 +0300

    api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -47,13 +47,13 @@ class ApiController extends Controller
     public function getFromLocal(Request $request)
     {
         $data = $request->data;
-       // Storage::put('messages4.txt', $data);
+        Storage::put('messages4.txt', $data[0]);
         //Storage::put('messages1.txt', json_encode($data, JSON_PRETTY_PRINT));
         //$data=json_encode($data, JSON_PRETTY_PRINT);
-        foreach($data as $oneRow)
-        {
-            $this->readyResults->test($oneRow);
-        }
+//        foreach($data as $oneRow)
+//        {
+//            $this->readyResults->test($oneRow);
+//        }
         return response()->json([
             'message' => 'Данные успешно получены',
             'received_data' => 'done'
