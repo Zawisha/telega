ea35b3f315f59fd2e797232e17ae323a46d81f53
zawisha <ogniemimechem@gmail.com>
2024-09-27 14:38:08 +0300

    api fix

--- resources/js/components/Homemain.vue
+++ resources/js/components/Homemain.vue
@@ -1,14 +1,19 @@
 <template>
     <div class="container main_buttons">
-        <button type="button" class="btn btn-primary otst_but" @click="goToLink('telegramNavi')">Инвайт в телеге</button>
-        <button type="button" class="btn btn-primary otst_but" @click="goToLink('checkClient')">Кому уже писал</button>
-        <button type="button" class="btn btn-primary otst_but" @click="goToLink('searchNavi')">Поиск клиентов телеграм</button>
-        <button type="button" class="btn btn-primary otst_but" @click="goToLink('vk')">ВК</button>
+        <button type="button" class="btn btn-primary otst_but" @click="goToLink('telegramNavi')" v-if="isHomePage">Инвайт в телеге</button>
+        <button type="button" class="btn btn-primary otst_but otst_mob_top" @click="goToLink('checkClient')"v-if="!isHomePage">Кому уже писал</button>
+        <button type="button" class="btn btn-primary otst_but otst_mob_top" @click="goToLink('searchNavi')">Поиск клиентов телеграм</button>
+        <button type="button" class="btn btn-primary otst_but otst_mob_top" @click="goToLink('vk')">ВК</button>
     </div>
 </template>
 
 <script>
 export default {
+    computed: {
+        isHomePage() {
+            return window.location.pathname === 'http://yourlocaldomain.local/sendHosting'; // Проверка, что текущий URL - это главная страница
+        }
+    },
     methods: {
     }
 }
