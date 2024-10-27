11dcfce540b0146dd47c201990d48726c73e8370
zawisha <ogniemimechem@gmail.com>
2024-10-27 06:18:29 +0300

    fix front

--- resources/js/components/Homemain.vue
+++ resources/js/components/Homemain.vue
@@ -3,7 +3,7 @@
         <button type="button" class="btn btn-primary otst_but" @click="goToLink('telegramNavi')" v-if="isHomePage">Инвайт в телеге</button>
         <button type="button" class="btn btn-primary otst_but otst_mob_top" @click="goToLink('checkClient')"v-if="!isHomePage">Кому уже писал</button>
         <button type="button" class="btn btn-primary otst_but otst_mob_top" @click="goToLink('searchNavi')">Поиск клиентов</button>
-        <button type="button" class="btn btn-primary otst_but otst_mob_top" @click="goToLink('vk')">ВК</button>
+        <button type="button" class="btn btn-primary otst_but otst_mob_top" @click="goToLink('vk')" v-if="isHomePage">ВК</button>
     </div>
 </template>
 
@@ -11,7 +11,7 @@
 export default {
     computed: {
         isHomePage() {
-            return window.location.pathname === 'http://yourlocaldomain.local/sendHosting'; // Проверка, что текущий URL - это главная страница
+            return window.location == 'http://yourlocaldomain.local/'; // Проверка, что текущий URL локалка
         }
     },
     methods: {
